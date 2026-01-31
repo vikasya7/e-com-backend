2bd086b7a18c28835c6bcaa504b7fb27240a7bb5
vikasya7 <vikasyaa19@gmail.com>
2026-01-31 11:22:03 +0530

    feat: add cloudinary integration for file upload and deletion

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,42 @@
+import {v2 as cloudinary} from 'cloudinary'
+import dotenv from 'dotenv'
+import fs from 'fs'
+dotenv.config()
+cloudinary.config({
+    cloud_name:process.env.CLOUDINARY_CLOUD_NAME,
+    api_key:process.env.CLOUDINARY_API_KEY,
+    api_secret:process.env.CLOUDINARY_SECRET_NAME
+})
+
+
+const uploadOnCloudinary =async(localFilePath)=>{
+    try {
+        if(!localFilePath) return null;
+        const response=await cloudinary.uploader.upload(
+            localFilePath, {
+                resource_type:"auto"
+            }
+        )
+        console.log("File uploaded on cloudinary,File src:",+response.url);
+        fs.unlinkSync(localFilePath)
+        return response
+    } catch (error) {
+        console.log("Error on cloudinary",error);
+        fs.unlinkSync(localFilePath)
+        return null
+    }
+}
+
+
+const deleteFromCloudinary = async (publicId) =>{
+    try {
+        const result=await cloudinary.uploader.destroy(publicId)
+        console.log("deleted from cloudinary publicId:",publicId);
+        
+    } catch (error) {
+        console.log("error deleting from cloudinary",error);
+        return null;
+    }
+}
+
+export {uploadOnCloudinary,deleteFromCloudinary}
