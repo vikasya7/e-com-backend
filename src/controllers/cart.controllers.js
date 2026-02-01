e116dec901b85401f091c6221beda5df2e7ce6b1
vikasya7 <vikasyaa19@gmail.com>
2026-02-01 16:59:34 +0530

    fix: correct itemId validation and improve cart item handling logic

--- src/controllers/cart.controllers.js
+++ src/controllers/cart.controllers.js
@@ -1,8 +1,54 @@
-import { asyncHandler } from "../utils/asyncHandler";
+import { asyncHandler } from "../utils/asyncHandler.js";
+import {apiError} from "../utils/ApiError.js"
+import {Item} from "../models/item.models.js"
+import { Cart } from "../models/cart.models.js";
+import { ApiResponse } from "../utils/ApiResponse.js";
 
 
+const addToCart=asyncHandler(async(req ,res)=>{
+    const {itemId,quantity=1}=req.body
 
+    if(itemId){
+        throw new apiError(400,"itemId is required")
+    }
 
-const addToCari=asyncHandler(async(req ,res)=>{
+    // check item
+    const item = await Item.findById(itemId);
+
+    if(!item){
+        throw new apiError(400,"item not found")
+    }
     
+    let cart=Cart.findById(req.user._id);
+
+    if(!cart){
+        cart=await Cart.create({
+            user:req.user._id,
+            items:[]
+        })
+    }
+
+    const existingIndex=cart.items.findIndex(p=>p.itemId===itemId)
+
+    if(existingIndex>-1){
+        // items are there in the cart
+        let existingItem=cart.items[existingIndex];
+        existingItem.quantity+=quantity;
+        cart.items[existingIndex]=existingItem
+    }
+    else{
+        cart.items.push({itemId,quantity})
+    }
+
+    // total price
+    cart.totalPrice=cart.items.reduce(
+        (acc,curr)=>acc+curr.price*curr.quantity,0
+    )
+
+
+    await cart.save();
+    
+     return res.status(200).json(
+        new ApiResponse(200, cart, "Item added to cart successfully")
+    );
 })
