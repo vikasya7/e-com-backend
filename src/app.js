e6fb7c441a23092ad9ccf45e624848bb19215eac
vikasya7 <vikasyaa19@gmail.com>
2026-02-07 17:05:30 +0530

    fix: update CORS configuration to use environment variable for origin

--- src/app.js
+++ src/app.js
@@ -3,12 +3,12 @@ import express from 'express'
 import cookieParser from 'cookie-parser'
 const app=express()
 
-app.use(
-    cors({
-         origin:process.env.CORS_ORIGIN,
-         credentials:true
-    })
-)
+
+
+app.use(cors({
+  origin: "http://localhost:5173",
+  credentials: true
+}));
 
 // common middlewares
 app.use(express.json({limit:"16kb"}))
